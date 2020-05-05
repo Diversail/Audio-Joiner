4fba1710cd86a650660364d59421c0e8ef942d14
Diversail <63429420+Diversail@users.noreply.github.com>
2020-05-05 17:05:28 -0400

    Improved design

--- Mix.py
+++ Mix.py
@@ -1,28 +1,38 @@
 from pydub import AudioSegment
-import easygui, time
-a=easygui.enterbox('How Many Music Tracks do you want to mix?' )
-a=int(a)
+import easygui,time,os
 z=[]
 c=0
 q=[]
 v=[]
-for x in range(0,a):
-    b=easygui.fileopenbox()
-    print(b)
-    if b==None:
-        print('Error')
-        a+=1
-    else:
-        z.append(b)
-    time.sleep(.5)
-for x in range(0,a):
-    c+=a
-    c=AudioSegment.from_mp3(z[x])
-    q.append(c)
-    print('%s is finished'% z[x])
-output=q[0]
-for x in range(0,a-1):
-    print('Recoding %s'% z[x])
-    a-=1
-    output = output.overlay(q[a], position=0)
-output.export("mixed_sounds.mp3", format="mp3")
+w=0
+m=0
+def Main():
+    global a,c,q,z
+    a=easygui.enterbox('How Many Music Tracks do you want to mix?' )
+    a=int(a)
+    for x in range(0,a):
+        b=easygui.fileopenbox()
+        while b==None:
+            b=easygui.fileopenbox()
+        print(b)
+        p=os.path.getsize(b)
+        if p>10000000:
+            m=1
+            print('File to large, try under 10 MB')
+        else:
+            z.append(b)
+    so = sorted(z, key = os.path.getsize)
+    so.reverse()
+    print(so)
+    for x in range(0,a):
+        c+=a
+        c=AudioSegment.from_mp3(so[x])
+        q.append(c)
+        print('%s is finished'% so[x])
+    output=q[0]
+    for x in range(0,a-1):
+        a-=1
+        print('Recoding %s'% so[a])
+        output = output.overlay(q[a], position=0)
+    output.export("mixed_sounds.mp3", format="mp3")
+Main()
